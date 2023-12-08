9203ae46a6745880c630015da9b935b58c7c6da6
Tien Duc <dotienduc201@gmail.com>
2023-12-08 10:29:23 +0700

    update file

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
-import SlideIndex from "@/components/Layout/Slide/SlideIndex";
+import SlideIndex from '@/components/Layout/Slide/SlideIndex';
+import BestProducts from '@/components/Layout/BestProducts/BestProductsIndex';
 
 const HomePage = () => {
-    return ( 
-        <SlideIndex/>
-     );
-}
+    return (
+        <>
+            <SlideIndex />
+            <BestProducts />
+        </>
+    );
+};
 
 export default HomePage;
