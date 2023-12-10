a7251fea702d8d5039914bd0cc70f3c617c55361
Tien Duc <dotienduc201@gmail.com>
2023-12-10 19:49:45 +0700

    .

--- src/components/Layout/Footer/FooterIndex.js
+++ src/components/Layout/Footer/FooterIndex.js
@@ -16,79 +16,91 @@ function FooterIndex() {
 
     return (
         <div className={cx('footer-container')}>
-            <div
-                className={cx('footer-back-ground')}
-                style={urlBackgroundImage}
-            >
-                <ul className={cx('footer-wrapper')}>
-                    <h4 className={cx('footer-title')}>About us</h4>
-                    <li className={cx('footer-detail')}>
-                        <span className={cx('footer__description')}>
-                            Pellentesque posuere orci lobortis scelerisque
-                            blandit. Donec id tellus lacinia an, tincidunt risus
-                            ac, consequat velit.
-                        </span>
-                    </li>
-                    <li className={cx('footer-outner-btn')}>
-                        <Link to="/" className={cx('footer-btn')}>
-                            Read More
-                        </Link>
-                    </li>
-                    <li className={cx('footer-outner-icon')}>
-                        <span>
-                            <FaceBook />
-                            <Link to="/" />
-                        </span>
-                        <span>
-                            <Pinterest />
-                            <Link to="/" />
-                        </span>
-                        <span>
-                            <Instagram />
-                            <Link to="/" />
-                        </span>
-                    </li>
-                </ul>
+            <div className={cx('footer-back-ground')} style={urlBackgroundImage}>
+                <div className={cx('footer-main')}>
+                    <ul className={cx('footer-wrapper')}>
+                        <h4 className={cx('footer-title')}>About us</h4>
+                        <li className={cx('footer-detail')}>
+                            <span className={cx('footer-description')}>
+                                Pellentesque posuere orci lobortis scelerisque blandit. Donec id tellus lacinia an,
+                                tincidunt risus ac, consequat velit.
+                            </span>
+                        </li>
+                        <li className={cx('footer-outner-btn')}>
+                            <Link to="/" className={cx('footer-btn')}>
+                                Read More
+                            </Link>
+                        </li>
+                        <li className={cx('footer-outner-icon')}>
+                            <span>
+                                <FaceBook />
+                                <Link to="/" />
+                            </span>
+                            <span>
+                                <Pinterest />
+                                <Link to="/" />
+                            </span>
+                            <span>
+                                <Instagram />
+                                <Link to="/" />
+                            </span>
+                        </li>
+                    </ul>
 
-                {/* code item */}
-                
+                    <FooterItem
+                        title={'Help'}
+                        item_1={'Search'}
+                        item_2={'Help'}
+                        item_3={'Information'}
+                        item_4={'Privacy Policy'}
+                        item_5={'Shipping Details'}
+                    />
+                    <FooterItem
+                        title={'Support'}
+                        item_1={'Contact us'}
+                        item_2={'About us'}
+                        item_3={'Careers'}
+                        item_4={'Refunds'}
+                        item_5={'Deliveries'}
+                    />
+                    <FooterItem
+                        title={'Information'}
+                        item_1={'Search Terms'}
+                        item_2={'Advanced Search'}
+                        item_3={"Help & Faq's"}
+                        item_4={'Store Location'}
+                        item_5={'Orders & Returns'}
+                    />
 
-                <FooterItem/>
-                <FooterItem/>
-                <FooterItem/>
-                
-                <ul className={cx('footer-wrapper')}>
-                    <h4 className={cx('footer-title')}>About us</h4>
-                    <li className={cx('footer-detail')}>
-                        <span className={cx('footer__description')}>
-                            Pellentesque posuere orci lobortis scelerisque
-                            blandit. Donec id tellus lacinia an, tincidunt risus
-                            ac, consequat velit.
-                        </span>
-                    </li>
-                    <li className={cx('footer-outner-btn')}>
-                        <Link to="/" className={cx('footer-btn')}>
-                            Read More
-                        </Link>
-                    </li>
-                    <li className={cx('footer-outner-icon')}>
-                        <span>
-                            <FaceBook />
-                            <Link to="/" />
-                        </span>
-                        <span>
-                            <Pinterest />
-                            <Link to="/" />
-                        </span>
-                        <span>
-                            <Instagram />
-                            <Link to="/" />
-                        </span>
-                    </li>
-                </ul>
-
-               
-
+                    <ul className={cx('footer-wrapper')}>
+                        <h4 className={cx('footer-title')}>Contact us</h4>
+                        <li className={cx('footer-detail')}>
+                            <span className={cx('footer__description')}>
+                                Pellentesque posuere orci lobortis scelerisque blandit. Donec id tellus lacinia an,
+                                tincidunt risus ac, consequat velit.
+                            </span>
+                        </li>
+                        <li className={cx('footer-outner-btn')}>
+                            <Link to="/" className={cx('footer-btn')}>
+                                Read More
+                            </Link>
+                        </li>
+                        <li className={cx('footer-outner-icon')}>
+                            <span>
+                                <FaceBook />
+                                <Link to="/" />
+                            </span>
+                            <span>
+                                <Pinterest />
+                                <Link to="/" />
+                            </span>
+                            <span>
+                                <Instagram />
+                                <Link to="/" />
+                            </span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/components/Layout/Footer/FooterItem.js
+++ src/components/Layout/Footer/FooterItem.js
@@ -3,16 +3,16 @@ import styles from './FooterIndex.module.scss';
 
 const cx = classNames.bind(styles);
 
-function FooterItem() {
+function FooterItem({title,item_1,item_2,item_3,item_4,item_5}) {
     return (
         <div className={cx('footer-item')}>
-            <h4 className={cx('footer-item-title')}>Help</h4>
+            <h4 className={cx('footer-item-title')}>{title}</h4>
             <ul className={cx('footer-item-wrapper')}>
-                <li className={cx('footer-item-child')}>Search</li>
-                <li className={cx('footer-item-child')}>Help</li>
-                <li className={cx('footer-item-child')}>Information</li>
-                <li className={cx('footer-item-child')}>Privacy Policy</li>
-                <li className={cx('footer-item-child')}>Shipping Details</li>
+                <li className={cx('footer-item-child')}>{item_1}</li>
+                <li className={cx('footer-item-child')}>{item_2}</li>
+                <li className={cx('footer-item-child')}>{item_3}</li>
+                <li className={cx('footer-item-child')}>{item_4}</li>
+                <li className={cx('footer-item-child')}>{item_5}</li>
             </ul>
         </div>
     );
