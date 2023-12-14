10058a3c155ed01eede65ed1d111d53e6c56e77c
Tien Duc <dotienduc201@gmail.com>
2023-12-15 02:32:48 +0700

    "Complete Benefit"

--- src/components/Layout/Main/Benefit/BenefitIndex.js
+++ src/components/Layout/Main/Benefit/BenefitIndex.js
@@ -1,18 +1,23 @@
 import classNames from 'classnames/bind';
 import styles from './BenefitIndex.module.scss';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function BenefitIndex({ title, description, icon, classNames = false }) {
+    const handleClickTitle = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
         <div className={cx('benefit-container')}>
             <div className={cx('benefit-outner-icon')}>
-                <div className={cx('benefit-icon')}>
-                    {icon}
-                </div>
+                <div className={cx('benefit-icon')}>{icon}</div>
             </div>
             <div className={cx('benefit-wrapper')}>
-                <h3 className={cx('benefit-title')}>{title}</h3>
+                <Link to="/" className={cx('benefit-title')} onClick={handleClickTitle}>
+                    {title}
+                </Link>
                 <p className={cx('benefit-description')}>{description}</p>
             </div>
         </div>
