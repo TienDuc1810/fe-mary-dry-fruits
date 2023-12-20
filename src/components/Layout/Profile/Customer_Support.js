71e791a2059758385e75d4756c112e7fd0ed357e
Duc <dotienduc201@gmail.com>
2023-12-20 06:09:06 -0800

    .

--- src/components/Layout/Profile/Customer_Support.js
+++ src/components/Layout/Profile/Customer_Support.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './Profile.module.scss';
+import Button from '@/components/Button/ButtonIndex';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +8,14 @@ const CustomerSupport = () => {
     return (
         <div>
             <h2 className={cx('profile-detail-heading')}>Customer Support</h2>
-            
+            <div className={cx('profile-help')}>
+            <label for="help" className={cx('profile-help-heading')}>Please send us your problem:</label>
+            <textarea id="help" className={cx('profile-help-textarea')} rows="7" cols="80">
+            </textarea>
+            </div>
+            <div className={cx('profile-help-btn')}>
+                <Button text={'Send'} blackText/>
+            </div>
         </div>
     );
 };
--- src/components/Layout/Profile/Notification.js
+++ src/components/Layout/Profile/Notification.js
@@ -1,5 +1,8 @@
 import classNames from 'classnames/bind';
 import styles from './Profile.module.scss';
+import Button from '@/components/Button/ButtonIndex';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +10,18 @@ const Notification = () => {
     return (
         <div>
             <h2 className={cx('profile-detail-heading')}>Notification</h2>
-                <p>Bạn chưa có thông báo</p>
+                <div className={cx('profile-noti')}>
+                    <span>
+                        <FontAwesomeIcon
+                            icon={icon({ name: 'bell', style: 'solid' })}
+                            className={cx('profile-icon')}
+                        />
+                    </span>
+                    <span className={cx('profile-text')}>You don't have notification </span>
+                </div>
+                <div className={cx('profile-btn')}>
+                    <Button text={'Continue Shopping'} blackText link={'/product'}/>
+                </div>
         </div>
     );
 };
