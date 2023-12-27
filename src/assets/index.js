1fbefe06d7fe55200954e4c6c4f75dcceab15a24
Tien Duc <dotienduc201@gmail.com>
2023-12-27 15:23:56 +0700

    .

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,14 +5,12 @@ const images = {
     category_2: require('@/assets/images/category_2.jpg'),
     category_3: require('@/assets/images/category_3.jpg'),
     logo: require('@/assets/images/logo.png'),
-    food: require('@/assets/images/food.png'),
+    
     visa: require('@/assets/images/visa.png'),
     paypal: require('@/assets/images/money.png'),
     mastercard: require('@/assets/images/card.png'),
-    check: require('@/assets/images/check.png'),
-    brand: require('@/assets/images/brand.webp'),
-    comment: require('@/assets/images/comment.png'),
-    like: require('@/assets/images/like.png'),
+    
+    
     nutritional: require('@/assets/images/nutritional.webp'),
     avatar: require('@/assets/images/Avatar.jpg'),
     avatar_default: require('@/assets/images/avatar-default.png'),
